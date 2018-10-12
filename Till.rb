f7d1295225199542d2c86e1261ec480b3d20e34c
Andryut <aahuertasca@unal.edu.co>
2018-10-12 17:54:38 -0500

    creating the till class

---
+++ Till.rb
@@ -0,0 +1,15 @@
+class Till
+
+  def initialize queue:, attendedList:
+    @queue = queue
+    @attendedList = attendedList
+  end
+
+  def checkout
+
+  end
+
+  protected
+  def update
+  end
+end
