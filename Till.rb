bc85dadb85b26439f2f7735bab0331641f0b2760
Andryut <aahuertasca@unal.edu.co>
2018-10-12 19:27:36 -0500

    Advance

--- Till.rb
+++ Till.rb
@@ -3,13 +3,27 @@ class Till
   def initialize queue:, attendedList:
     @queue = queue
     @attendedList = attendedList
+    unless queue.empty?
+      self.nextPurchase
+    end
   end
 
   def checkout
-
+    @atentionDelay -= 1
+    self.
   end
 
   protected
-  def update
+  def nextPurchase
+    @actualCustomer = @queue.remove
+    randomCreator = Random.new
+    @atentionDelay = randomCreator.rand(4..25)
+  end
+
+  def endPurchase
+    @attendedList << @actualCustomer
+    unless queue.empty?
+      self.nextPurchase
+    end
   end
 end
