9adabf078aa205c8282f735765f447eb48219dfb
jhonatan247 <yon.t.f@hotmail.com>
2018-10-15 15:52:23 -0500

    Input int format class

--- InputInterface.rb
+++ InputInterface.rb
@@ -16,4 +16,26 @@ class InputFormat
 	end
 end
 
+class InputIntFormat < InputFormat
+	attr_accessor :errorMessage
+	def initialize message:"Enter a value", errorMessage: "The value entered does not have the correct format"
+		super message
+		@errorMessage = errorMessage
+	end
+	def execute
+		isCorrect = false
+		value = 0
+		until isCorrect
+			print(message)
+			input = gets.chomp()
+			if InputFormat.is_i? input
+				value = input.to_i
+				isCorrect = true
+			else
+				puts(@errorMessage)
+			end
+		end 
+		return value
+	end
+end
 
