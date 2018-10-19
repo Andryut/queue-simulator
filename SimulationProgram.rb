e653bcc09b0b79403e389c268ccef92ede42ce15
Andryut <aahuertasca@unal.edu.co>
2018-10-19 13:50:24 -0500

    Correction of a parameter name.

--- SimulationProgram.rb
+++ SimulationProgram.rb
@@ -2,10 +2,10 @@ class SimulationProgram
 
   def initialize
     @input_interface = InputInterface.new
-    @input_interface.add_enum_input(options: ["Single queue.", "Multiple queue"], :simulation_type)
-    @input_interface.add_int_input(message: "Enter the number of active tills.", :active_queues)
-    @input_interface.add_int_input(message: "Enter the time of the simulation in minutes.", :simulation_time)
-    @input_interface.add_int_input(message: "Enter the delta of time in seconds.", :delta_time)
+    @input_interface.add_enum_input(options: ["Single queue.", "Multiple queue"], key: :simulation_type)
+    @input_interface.add_int_input(message: "Enter the number of active tills.", key: :active_queues)
+    @input_interface.add_int_input(message: "Enter the time of the simulation in minutes.", key: :simulation_time)
+    @input_interface.add_int_input(message: "Enter the delta of time in seconds.", key: :delta_time)
   end
 
   def start
